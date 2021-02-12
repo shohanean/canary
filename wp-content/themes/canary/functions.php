3b766e9c36591115a4e904007f2712d356ada4ce
shohanean <hossain_shohan1@live.com>
2021-02-12 15:01:53 +0600

    all design

--- wp-content/themes/canary/functions.php
+++ wp-content/themes/canary/functions.php
@@ -2,9 +2,12 @@
 function canary_scripts() {
 	wp_enqueue_style( 'canary-style', get_stylesheet_uri(), array(), 1.0 );	
 	wp_enqueue_style( 'bootstrap-css', get_template_directory_uri() . '/assets/css/bootstrap.min.css', array(), 1.0 );	
+	wp_enqueue_style( 'slick-css', get_template_directory_uri() . '/assets/css/slick.css', array(), 1.0 );	
 
 	wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/assets/js/bootstrap.min.js', array('jquery'), 1.0, true );
 	wp_enqueue_script( 'popper-js', get_template_directory_uri() . '/assets/js/popper.min.js', array('jquery'), 1.0, true );
+	wp_enqueue_script( 'custom-js', get_template_directory_uri() . '/assets/js/custom.js', array('jquery'), 1.0, true );
+	wp_enqueue_script( 'slick-js', get_template_directory_uri() . '/assets/js/slick.min.js', array('jquery'), 1.0, true );
 	
 }
 add_action( 'wp_enqueue_scripts', 'canary_scripts' );
--- wp-content/themes/canary/index.php
+++ wp-content/themes/canary/index.php
@@ -1,3 +1,262 @@
 <?php get_header(); ?>
-
+<!-- ===================================================
+                        Main Body Start
+=====================================================-->
+<section id="main_body">
+    <div class="container">
+        <div class="row">
+            <div class="col-8 pl-0">
+                <div class="main_body_banner">
+                    <img src="<?=get_template_directory_uri()?>/assets/images/banner.jpg" alt="<?=get_template_directory_uri()?>">
+                </div>
+                <div class="main_body_marque_one">
+                    <h2>
+                        <marquee behavior="" direction="left">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, eum? Fuga officia aut sint quis eaque quos iste sed optio natus? Inventore id quibusdam fuga blanditiis nemo qui in earum.
+                        </marquee>
+                    </h2>
+                </div>
+                <div class="main_body_marque_two pt-2 pb-2">
+                    <h6 class="text-danger font-weight-bold">
+                        <marquee behavior="" direction="left" scrollamount="12">
+                            বাংলা সব অক্ষর রাখার একটা চেষ্টা ছিল। কিন্তু তা ব্যর্থ – আমি শেষে এই চেষ্টা করাটাকেই অপ্রয়োজনীয় মনে করলাম। এ-তো আর বাংলা ভাষার আগার হচ্ছে না, এ হলো পরম্পরাহীন, কিংবা তাৎপর্যপূর্ণ একটি লেখা, যা বাংলা ভাষার প্রতিনিধিত্ব করবে টাইপসেটিং, প্রিন্টিং, ইন্ডাস্ট্রিতে কিংবা ওয়েব ডিযাইনে।
+                        </marquee>
+                    </h6>
+                </div>
+                <div class="slick_slider">
+                    <div>
+                        <img src="<?=get_template_directory_uri()?>/assets/images/slider1.jpg" alt="">
+                    </div>
+                    <div>
+                        <img src="<?=get_template_directory_uri()?>/assets/images/slider2.jpg" alt="">
+                    </div>
+                    <div>
+                        <img src="<?=get_template_directory_uri()?>/assets/images/slider3.jpg" alt="">
+                    </div>
+                </div>
+                <div class="initiative">
+                    <h4 class="purple_header">উদ্যোগ</h4>
+                    <ul>
+                        <li>
+                            <a href="">বাংলাদেশ সরকারের সপ্তম পঞ্চবার্ষিক পরিকল্পনা (২০১৬-২০২০)</a>
+                        </li>
+                        <li>
+                            <a href="">বাংলাদেশ সরকারের সপ্তম পঞ্চবার্ষিক পরিকল্পনা (২০১৬-২০২০)</a>
+                        </li>
+                        <li>
+                            <a href="">বাংলাদেশ সরকারের সপ্তম পঞ্চবার্ষিক পরিকল্পনা (২০১৬-২০২০)</a>
+                        </li>
+                        <li>
+                            <a href="">বাংলাদেশ সরকারের সপ্তম পঞ্চবার্ষিক পরিকল্পনা (২০১৬-২০২০)</a>
+                        </li>
+                        <li>
+                            <a href="">বাংলাদেশ সরকারের সপ্তম পঞ্চবার্ষিক পরিকল্পনা (২০১৬-২০২০)</a>
+                        </li>
+                        <li>
+                            <a href="">বাংলাদেশ সরকারের সপ্তম পঞ্চবার্ষিক পরিকল্পনা (২০১৬-২০২০)</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="links">
+                    <div class="row">
+                        <div class="col-4">
+                            <img src="<?=get_template_directory_uri()?>/assets/images/link1.png" alt="">
+                        </div>
+                        <div class="col-4">
+                            <img src="<?=get_template_directory_uri()?>/assets/images/link2.png" alt="">
+                        </div>
+                        <div class="col-4">
+                            <img src="<?=get_template_directory_uri()?>/assets/images/link3.png" alt="">
+                        </div>
+                        <div class="col-4">
+                            <img src="<?=get_template_directory_uri()?>/assets/images/link4.png" alt="">
+                        </div>
+                        <div class="col-4">
+                            <img src="<?=get_template_directory_uri()?>/assets/images/link5.png" alt="">
+                        </div>
+                        <div class="col-4">
+                            <img src="<?=get_template_directory_uri()?>/assets/images/link6.png" alt="">
+                        </div>
+                        <div class="col-4">
+                            <img src="<?=get_template_directory_uri()?>/assets/images/link7.png" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="popular">
+                    <h4 class="purple_header">জনপ্রিয়</h4>
+                    <div class="row">
+                        <div class="col-4">
+                            <h6>সেবাসমূহ</h6>
+                            <ul>
+                                <li>
+                                    <a href="">বাংলাদেশ সরকারের (২০১৬-২০২০)</a>
+                                </li>
+                                <li>
+                                    <a href="">বাংলাদেশ সরকারের (২০১৬-২০২০)</a>
+                                </li>
+                                <li>
+                                    <a href="">বাংলাদেশ সরকারের (২০১৬-২০২০)</a>
+                                </li>
+                                <li>
+                                    <a href="">বাংলাদেশ সরকারের (২০১৬-২০২০)</a>
+                                </li>
+                                <li>
+                                    <a href="">বাংলাদেশ সরকারের (২০১৬-২০২০)</a>
+                                </li>
+                                <li>
+                                    <a href="">বাংলাদেশ সরকারের (২০১৬-২০২০)</a>
+                                </li>
+                                <li>
+                                    <a href="">বাংলাদেশ সরকারের (২০১৬-২০২০)</a>
+                                </li>
+                                <li>
+                                    <a href="">বাংলাদেশ সরকারের (২০১৬-২০২০)</a>
+                                </li>
+                                <li>
+                                    <a href="">বাংলাদেশ সরকারের (২০১৬-২০২০)</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-4">
+                            <h6>পাতাসমূহ</h6>
+                            <ul>                                    
+                                <li>
+                                    <a href="">সপ্তম পঞ্চবার্ষিক পরিকল্পনা</a>
+                                </li>
+                                <li>
+                                    <a href="">সপ্তম পঞ্চবার্ষিক পরিকল্পনা</a>
+                                </li>
+                                <li>
+                                    <a href="">সপ্তম পঞ্চবার্ষিক পরিকল্পনা</a>
+                                </li>
+                                <li>
+                                    <a href="">সপ্তম পঞ্চবার্ষিক পরিকল্পনা</a>
+                                </li>
+                                <li>
+                                    <a href="">সপ্তম পঞ্চবার্ষিক পরিকল্পনা</a>
+                                </li>
+                                <li>
+                                    <a href="">সপ্তম পঞ্চবার্ষিক পরিকল্পনা</a>
+                                </li>
+                                <li>
+                                    <a href="">সপ্তম পঞ্চবার্ষিক পরিকল্পনা</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-4">
+                            <h6>পোর্টালসমূহ</h6>
+                            <ul>
+                                <li>
+                                    <a href="">বাংলাদেশ</a>
+                                </li>
+                                <li>
+                                    <a href="">বাংলাদেশ সরকারের সপ্তম পঞ্চবার্ষিক পরিকল্পনা (২০১৬-২০২০)</a>
+                                </li>
+                                <li>
+                                    <a href="">বাংলাদেশ</a>
+                                </li>
+                                <li>
+                                    <a href="">বাংলাদেশ সরকারের সপ্তম পঞ্চবার্ষিক পরিকল্পনা (২০১৬-২০২০)</a>
+                                </li>
+                                <li>
+                                    <a href="">বাংলাদেশ</a>
+                                </li>
+                                <li>
+                                    <a href="">বাংলাদেশ সরকারের সপ্তম পঞ্চবার্ষিক পরিকল্পনা (২০১৬-২০২০)</a>
+                                </li>
+                                <li>
+                                    <a href="">বাংলাদেশ</a>
+                                </li>
+                                <li>
+                                    <a href="">বাংলাদেশ সরকারের সপ্তম পঞ্চবার্ষিক পরিকল্পনা (২০১৬-২০২০)</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4 pr-0 right_bar">
+                <div class="mb-3">
+                    <p>
+                        মাস্ক পরুন সেবা নিন
+                    </p>
+                    <img src="<?=get_template_directory_uri()?>/assets/images/widget1.jpeg" alt="">
+                </div>
+                <div class="mb-3">
+                    <p>
+                        আশ্রয়ণের অধিকার শেখ হাসিনার উপহার
+                    </p>
+                    <iframe width="100%" height="315" src="https://www.youtube.com/embed/x50HVEENBhc" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe width="100%" height="315" src="https://www.youtube.com/embed/FvkvWDDsBq4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe width="100%" height="315" src="https://www.youtube.com/embed/iJECjK64jaI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe width="100%" height="315" src="https://www.youtube.com/embed/x50HVEENBhc" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe width="100%" height="315" src="https://www.youtube.com/embed/FvkvWDDsBq4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe width="100%" height="315" src="https://www.youtube.com/embed/iJECjK64jaI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe width="100%" height="315" src="https://www.youtube.com/embed/x50HVEENBhc" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe width="100%" height="315" src="https://www.youtube.com/embed/FvkvWDDsBq4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe width="100%" height="315" src="https://www.youtube.com/embed/iJECjK64jaI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>                
+        </div>
+    </div>
+</section>
+<!-- ===================================================
+                        Main Body End
+=====================================================-->
+<!-- ===================================================
+                        Footer End
+=====================================================-->
+<section id="footer_part">
+    <div class="container footer_artwork">
+        <img src="<?=get_template_directory_uri()?>/assets/images/footer_top_bg.png" alt="">
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-7">
+                <nav class="navbar navbar-expand-lg p-0">
+                    <div class="container nav_container">            
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNav">
+                            <ul class="navbar-nav">
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Features</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Features</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+            <div class="col-5 pt-3">
+                <small>
+                    পরিকল্পনা ও বাস্তবায়নে: এটুআই, মন্ত্রিপরিষদ বিভাগ, বিসিসি, বেসিস, ডিওআইসিটি                    
+                </small>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-7 pt-3 pb-3">
+                <small>
+                    সাইটটি শেষ হাল-নাগাদ করা হয়েছে: ২০২১-০১-৩০ ০৫:১৯:৪৬
+                </small>
+            </div>
+            <div class="col-5">
+                <small>
+                    কারিগরি সহায়তায়:
+                </small>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- ===================================================
+                        Footer End
+=====================================================-->
 <?php get_footer(); ?>
