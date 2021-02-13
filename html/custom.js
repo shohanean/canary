d41869fa6409228f1f9b28fb2c1f530bfb353ea9
shohanean <hossain_shohan1@live.com>
2021-02-13 09:06:56 +0600

    with html

---
+++ html/custom.js
@@ -0,0 +1,9 @@
+jQuery(function($){
+    $('.slick_slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+    });
+});
